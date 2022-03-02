e04dbd3a0a78ce21a78014366a006cdd89bc3295
smartcoder80 <53383205+smartcoder80@users.noreply.github.com>
2022-03-02 06:30:05 +0530

    Add files via upload

---
+++ DbHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+
+namespace BulkOperationDemo
+{
+    public class DbHelper
+    {
+        
+        public static List<Product> GetProductCatalog()
+        {
+            List<Product> result;
+            using (var reader = new StreamReader("path\\to\\file.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                result = csv.GetRecords<Product>().ToList();
+            }
+
+            return result;
+        }
+       
+        private static List<KeyWord> GetKeyWordList(string csvKeyWordString)
+        {
+            List<KeyWord> keyWordObjList = new List<KeyWord>();
+            if (!string.IsNullOrEmpty(csvKeyWordString))
+            {
+                List<string> keyWordStringArray = csvKeyWordString.Split(',').ToList();
+                foreach (string keywordString in keyWordStringArray)
+                {
+                    KeyWord KeyWord = new KeyWord()
+                    {
+                        input = new List<string>() { keywordString }
+                    };
+                    keyWordObjList.Add(KeyWord);
+                }
+            }
+            return keyWordObjList;
+        }
+    }
+}
---
+++ ElasticsearchHelper.cs
@@ -0,0 +1,53 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkOperationDemo
+{
+    public static class ElasticsearchHelper
+    {
+        public static ElasticClient GetESClient()
+        {
+            ConnectionSettings connectionSettings;
+            ElasticClient elasticClient;
+            StaticConnectionPool connectionPool;
+            var nodes = new Uri[] {
+                new Uri("http://da6591de6c48.ngrok.io") //Provide ES cluster addresses)
+            };
+            connectionPool = new StaticConnectionPool(nodes);
+            connectionSettings = new ConnectionSettings(connectionPool);
+            elasticClient = new ElasticClient(connectionSettings);
+
+            return elasticClient;
+        }
+
+        public static void CreateBulkDocument(ElasticClient elasticClient, string indexName, List<Product> products)
+        {
+            var bulkAllObservable = elasticClient.BulkAll(products, b => b
+    .Index("myindex")
+    // how long to wait between retries
+    .BackOffTime("30s")
+    // how many retries are attempted if a failure occurs
+    .BackOffRetries(2)
+    // refresh the index once the bulk operation completes
+    .RefreshOnCompleted()
+    // how many concurrent bulk requests to make
+    .MaxDegreeOfParallelism(100)
+    // number of items per bulk request
+    .Size(1000)
+)
+// Perform the indexing, waiting up to 15 minutes. 
+// Whilst the BulkAll calls are asynchronous this is a blocking operation
+.Wait(TimeSpan.FromMinutes(15), next =>
+{
+    // do something on each response e.g. write number of batches indexed to console
+});
+
+        }
+
+    }
+}
